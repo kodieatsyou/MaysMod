6b16c5921d941b1545168e87eaf718b4f1747e1f
Tanner May <tannerjmay@gmail.com>
2019-12-27 22:54:06 -0700

    Changed Golf Ball Behavior

--- Projectiles/GolfBall.cs
+++ Projectiles/GolfBall.cs
@@ -1,4 +1,9 @@
+
+using Microsoft.Xna.Framework;
+using System;
+using System.Diagnostics;
 using Terraria.ID;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace MaysMod.Projectiles
@@ -12,16 +17,35 @@ namespace MaysMod.Projectiles
 
 		public override void SetDefaults()
 		{
-			projectile.arrow = true;
-			projectile.width = 24;
-			projectile.height = 24;
-			projectile.aiStyle = 8;
+			projectile.width = 12;
+			projectile.height = 12;
 			projectile.friendly = true;
 			projectile.melee = true;
-			projectile.ignoreWater = true;
-			aiType = ProjectileID.Fireball;
+			projectile.aiStyle = 14;
+			aiType = ProjectileID.Glowstick;
 		}
 
-		// Additional hooks/methods here.
+		public override void AI()
+		{
+			projectile.ai[0] += 1f; // Use a timer to wait 15 ticks before applying gravity.
+			if (projectile.ai[0] >= 10f)
+			{
+				projectile.Kill();
+			}
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			Dust.NewDust(projectile.position, 20, 20, DustID.Dirt, 0, 0, 150, default(Color), 0.7f);
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			if(projectile.velocity.Y != 0)
+			{
+				Dust.NewDust(projectile.position, 25, 25, DustID.Dirt, 0, 0, 150, default(Color), 0.7f);
+			}
+			return base.OnTileCollide(oldVelocity);
+		}
 	}
 }
